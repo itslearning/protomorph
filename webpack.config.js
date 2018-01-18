18dc48e3f024d17de02d4dee42440fd2e28888c5
Gavin King <gavin.king@itslearning.com>
2018-01-18 12:44:07 +0100

    should build in consumers dist

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,7 @@ module.exports = {
     },
 
     output: {
-        path: path.join(__dirname, '/dist'),
+        path: path.join(process.cwd(), '/dist'),
         filename: '[name].bundle.js'
     },
 
