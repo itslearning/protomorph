366b498d1d99f2aa9b441e5547dfe22a5bcba6c6
Stephane Vanraes <stephane.vanraes@itslearning.com>
2019-12-09 13:39:09 +0100

    fix typo in rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -9,7 +9,7 @@ export default [
     */
     Svelte('./src/index.js', destination + 'index.bundle.js'),
     Svelte('./src/index.js', destination + 'index.bundle.es5.js', { legacy: true }),
-    /* Sass command takes an optional thrid arguments (options) with fields:
+    /* Sass command takes an optional third arguments (options) with fields:
         plugins: extra plugins to run after default ones
     */
     Sass('./src/theme.aaa.scss', destination + 'aaa.bundle.css'),
